b4a5ec3a23979bcf467925912f3774b9297e0d11
Dmitrij Waskowski <dymitr@gmail.com>
2016-09-09 15:55:20 +0200

    fixed for first start

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+# FF
+echo "deb http://packages.linuxmint.com debian import" > /etc/apt/sources.list.d/firefox.list
 apt-get -qq update
-apt-get -qq install curl nano git htop x-window-system-core kde kdm
-startkde
+apt-get -qq install firefox
+
+# all
+apt-get -qq install curl nano git htop
+
+# GUI with Gnome3
+apt-get -qq install aptitude tasksel
+tasksel install gnome-desktop --new-install
+apt-get -qq install gdm3
+service gdm start &
+
 
 
 
